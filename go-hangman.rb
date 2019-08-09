065096ebab0f9a1c635687be8344d5de2cdceec1
kumaraswamy chavvakula <kumarsparkz@gmail.com>
2019-08-09 16:11:02 -0400

    Update go-hangman.rb

--- go-hangman.rb
+++ go-hangman.rb
@@ -1,12 +1,12 @@
 # Classname should match the name of the installed package.
-class gohangman < Formula
+class Gohangman < Formula
     desc "Hangman is a game created using Go Lang."
     homepage "https://github.com/kumarsparkz/go-hangman"
     version "0.1.2"
 
     # Source code archive. Each tagged release will have one
     url "https://github.com/kumarsparkz/go-hangman/archive/{version}.tar.gz"
-    sha256 "9cca496b9b6caee80351b183c90ab98c0b5cb71e"
+    sha256 "f5ac2ba0dfc711d5b6c9121c6c51f11cc692c94d"
     
     depends_on "go" => :build
     
